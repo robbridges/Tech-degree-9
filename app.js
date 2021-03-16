9ec7ad564ac5ebb4ea1e173f7c16bee07b0c4491
robbridges <robbridges@users.noreply.github.com>
2021-03-16 15:00:05 -0700

    fixed imports

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
-const sequelize = require('sequelize');
+const { sequelize } = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
